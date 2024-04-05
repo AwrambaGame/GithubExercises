e1541918fda2657d1c49e4c729615239c45e7b46
Tekalig <tekahazi06@gmail.com>
2024-04-05 20:24:48 +0300

    add c# code and .net frame

---
+++ TekalignAnswers/Fraction.cs
@@ -0,0 +1,19 @@
+namespace TekalignAnswers
+{
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+}
---
+++ TekalignAnswers/PrintString.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string myString = "This is a game project";
+        Console.WriteLine(myString);
+    }
+}
