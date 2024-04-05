c0134c3cb39235ddaaf4246f312b5830cf9b1501
YishakG <111497293+YishakG@users.noreply.github.com>
2024-04-05 14:31:57 +0300

    Answers

---
+++ Questions/YishakAnswers/PrintNumbers.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FractionApp;
+
+namespace Helloworld
+{
+class Program{
+    static void Main(string[] args){
+        for (int i = 10; i <= 100; i+=10){
+            Console.WriteLine(i);
+            }
+        Fraction num1 = new Fraction(3,4);
+        Console.WriteLine(num1.ToString());
+
+    }
+}
+    
+}
+
+
