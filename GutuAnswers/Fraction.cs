79bcfbf7a264e685305140fdb116a79fa9b53a69
Gutu_R <adiregassa@gmail.com>
2024-04-05 13:32:58 +0000

    Answers

--- GutuAnswers/Fraction.cs
+++ GutuAnswers/Fraction.cs
@@ -23,14 +23,6 @@ namespace GutuAnswers
             return str;
         }
 
-        static void Main(string[] args)
-        {
-        //Test the Fraction class
-        Fraction fraction = new Fraction(1,2);
-        Console.WriteLine(fraction);
-
-        }
-
     }
 
     
