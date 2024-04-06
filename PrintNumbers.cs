77b782f87444251522df561247fc692bd37f01b0
Abrham111 <159109294+Abrham111@users.noreply.github.com>
2024-04-06 23:04:43 +0300

    Add files via upload

---
+++ PrintNumbers.cs
@@ -0,0 +1,14 @@
+using system;
+
+namespace loop {
+
+class introLoop {
+
+    public static void main(string[]args) {
+        for (int i = 10; i<=100; i+10 ) {
+            console.Writeline(i);
+        }
+
+    }
+}
+}
