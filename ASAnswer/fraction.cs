4af55fb6e18d1be107bed3f4d21c8c05e4391423
aschalew-mamo <162134580+aschalew-mamo@users.noreply.github.com>
2024-04-08 12:03:39 +0300

    taskdone

---
+++ ASAnswer/fraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Fraction
+{
+    public int Numerator;
+    public int Denominator;
+
+    // Constructor
+    public Fraction(int numerator, int denominator)
+    {
+        Numerator = numerator;
+        
+        Denominator = denominator;
+    }
+
+    // Override ToString method
+    public override string ToString()
+    {
+        return $"{Numerator}/{Denominator}";
+    }
+}
+
+
+
---
+++ ASAnswer/printString.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class printString
+{
+    static void Main(string[] args)
+    {
+        string myString = "gamer ";
+        Console.WriteLine(myString);
+    }
+}
