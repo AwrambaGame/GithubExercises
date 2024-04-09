2ab9c54ee2f528377b9cf48917c33f2dc1ef11fc
Dorka Alemayehu <dorkaalemayehu2@gmail.com>
2024-04-09 21:30:53 +0300

    Answer to exercises

---
+++ dorkaAnswers/TestFraction.cs
@@ -0,0 +1,10 @@
+﻿using Fraction;
+
+public class FractionTest
+{
+    static void Main(string[] args)
+    {
+        Fraction fraction = new Fraction(1, 2);
+        Console.WriteLine(fraction);
+    }
+}
